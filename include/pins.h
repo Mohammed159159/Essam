3a97331045a9f04378bd93d5d23a5b6c774b65db
Hany-R <hanyrafat07@gmail.com>
2024-11-25 22:43:42 +0200

    dev: Add S4, S5 & remove servo pins

--- include/pins.h
+++ include/pins.h
@@ -2,8 +2,6 @@
 #define PINS_H
 
 /* SERVO PINS */
-#define SERVO1 17
-#define SERVO2 16
 
 /* MOTOR PINS */
 #define IN1 11
@@ -14,13 +12,14 @@
 #define ENB 27
 #define MOTOR_PINS_NUM 9
 
-/* SHOOTER PINS */
-#define IN5 19
-#define ENC 18
+
 
 /* LINE FOLLOWER PINS */
 #define S1 25
 #define S2 26
 #define S3 27
+#define S4 28
+#define S5 29
+
 
 #endif
